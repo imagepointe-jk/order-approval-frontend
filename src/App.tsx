ebce4f1512128830d42d520addb715a6d9a7e876
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2023-11-02 11:49:53 -0500

    Add TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <p>Choo Choo! This is an example of a Vite + React app running on Railway.</p>
+        <p>Updated</p>
       </div>
     </>
   )
